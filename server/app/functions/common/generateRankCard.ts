35f965cf010d27035b35c84bca64ec254d3b2ac7
Ali Shadman <alishadman955@gmail.com>
2022-06-06 00:59:36 +0200

    font change

--- server/app/functions/common/generateRankCard.ts
+++ server/app/functions/common/generateRankCard.ts
@@ -1,6 +1,6 @@
 import canvas, { registerFont } from "canvas"; // For canvas.
 import { DiscordSettingsRankInterface } from "@app/types/databases.type";
-registerFont("app/fonts/Inter-Medium.ttf", { family: "InterBold" });
+registerFont("app/fonts/Inter-Regular.ttf", { family: "InterBold" });
 
 interface userInfo {
 	username: string;
